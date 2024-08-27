42901bdaf5c8d17e976dd54f9abfc46c392b759b
Frédérick FLORKOWSKI <f.florkowski@prysm.local>
2024-08-27 16:41:08 +0200

    added analytics form vercel to my website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import CursorFollower from "./components/ui/CursorFollower";
+import { Analytics } from "@vercel/analytics/react";
 
 const font = Manrope({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <CursorFollower />
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
